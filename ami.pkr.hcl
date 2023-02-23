761383f92655859a999e393596ed31583162027f
anvoruganti <voruganti.a@northeastern.edu>
2023-02-23 15:53:17 -0500

    fix : Assignment 4 - CI CD update

---
+++ ami.pkr.hcl
@@ -0,0 +1,75 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "source_ami" {
+  type    = string
+  default = "ami-0dfcb1ef8550277af" 
+  ## CHANGE THIS TO YOUR LUNX2 AMI ID
+}
+
+variable "ssh_username" {
+  type    = string
+  default = "ec2-user"
+
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-04ba5f58de0201e66"
+  ## CHANGE THIS TO YOUR SUBNET ID
+}
+
+# https://www.packer.io/plugins/builders/amazon/ebs
+source "amazon-ebs" "my-ami" {
+  region          = "${var.aws_region}"
+  ami_name        = "csye6225_${formatdate("YYYY_MM_DD_hh_mm_ss", timestamp())}"
+  ami_description = "AMI for CSYE 6225"
+  ami_regions = [
+    "us-east-1",
+  ]
+  ami_users = ["435677752779"] #Dev
+
+  aws_polling {
+    delay_seconds = 120
+    max_attempts  = 50
+  }
+
+
+  instance_type = "t2.micro"
+  source_ami    = "${var.source_ami}"
+  ssh_username  = "${var.ssh_username}"
+  subnet_id     = "${var.subnet_id}"
+
+  launch_block_device_mappings {
+    delete_on_termination = true
+    device_name           = "/dev/xvda" ## LINUX2 DEVICE
+    volume_size           = 8
+    volume_type           = "gp2"
+    encrypted             = false
+  }
+}
+
+
+
+build {
+  sources = ["source.amazon-ebs.my-ami"]
+
+  provisioner "shell" {
+    inline = ["mkdir -p /tmp/apps/"]
+  }
+
+  provisioner "file" {
+    ## THIS IS TO MOVE YOUR ZIP file to the EC2 instance 
+    source      = "webapp.zip"
+    destination = "/tmp/apps/webapp.zip"
+  }
+
+  provisioner "shell" {
+    ## THIS IS FOR SETUP & APPLICATION AS SERVICE
+    script = "script.sh"
+  }
+
+}
+
---
+++ script.sh
@@ -0,0 +1,91 @@
+# #!/bin/bash
+# ## Author: Anirudh Voruganti
+
+echo "|**********************  INSTALL SCRIPT **********************|"
+
+
+echo "|**********************  Upgrade OS Packages **********************|"
+sudo yum update
+sudo yum upgrade -y
+
+
+echo "|********************** Install NodeJS **********************|"
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+node -v
+
+echo "|********************** Install POSTGRESQL ********************** |"   
+sudo amazon-linux-extras enable postgresql14
+sudo yum install postgresql-server -y
+sudo PGSETUP_INITDB_OPTIONS=" --auth=trust" postgresql-setup --initdb --unit postgresql --debug
+
+
+echo "|**********************  Start POSTGRESQL **********************  |"   
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+sudo systemctl status postgresql
+sudo -u postgres psql
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'root';"
+sudo -u postgres psql -c "create database database_development;"
+
+sudo cat /var/lib/pgsql/data/pg_hba.conf
+
+
+
+echo "|********************** UNZIP WEBAPP ********************** |"
+
+
+echo "check webapp.zip"
+SOURCE_ZIP="/tmp/apps/webapp.zip"
+APP_FOLDER="/home/ec2-user"
+echo "----Checking if the file exists----"
+ls -a $SOURCE_ZIP
+
+
+echo "copying the webapp to -" $APP_FOLDER
+
+# APP_FOLDER="/var/www/html/webapp"
+sudo mkdir -p $APP_FOLDER
+unzip $SOURCE_ZIP -d $APP_FOLDER
+sudo cd $APP_FOLDER/webapp
+
+echo "listing webapp"
+ls -a $APP_FOLDER/webapp
+
+chmod -R 777 $APP_FOLDER/webapp
+echo "pwd" - $(pwd)
+
+echo "Check permissions $APP_FOLDER/webapp"
+ls -la $APP_FOLDER/webapp
+
+
+echo "|********************** INSTALL NPM PACKAGES **********************  |"
+
+cd webapp
+npm i 
+sudo npm uninstall bcrypt
+sudo npm install bcrypt@5.1.0
+sudo npm install bcryptjs@2.4.3
+sudo npm install dotenv@16.0.3
+sudo npm install esm@3.2.25
+sudo npm install express@4.18.2
+sudo npm install helmet@6.0.1
+sudo npm install mocha@10.2.0 -g 
+sudo npm install morgan@1.10.0
+sudo npm install mysql2@3.1.0
+sudo npm install nodemon@2.0.20
+sudo npm install pg@8.9.0
+sudo npm install sequelize@5.21.7
+sudo npm install supertest@6.3.3
+sudo npm install pm2 -g
+
+
+
+echo "| **********************  STARTING THE APPLICATION AS A SERVICE ********************** |"
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /home/ec2-user
+pm2 start src/mainServer.js
+#npm run dev 
+pm2 save
+
+pm2 list
