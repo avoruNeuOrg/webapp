d3c6b8f4f8c2a5d5cec0ae46b814112b4491715b
Anirudh Voruganti <91443922+anvoruganti@users.noreply.github.com>
2023-04-01 23:22:15 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -93,7 +93,7 @@ sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -
 # pm2 list
 
 
-# sudo cp /tmp/app.service /lib/systemd/system/app.service
-# sudo rm -rf /tmp/app.service
- sudo systemctl daemon-reload
+sudo cp /tmp/app.service /lib/systemd/system/app.service
+sudo rm -rf /tmp/app.service
+sudo systemctl daemon-reload
 
